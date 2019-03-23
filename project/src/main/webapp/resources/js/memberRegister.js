8a1f7f4c82ee937a4484f8dc77035f8cd45864da
kys <romg321@naver.com>
2019-03-24 05:58:46 +0900

    memberRegister.js 조금더 세분화 작업 완료

--- project/src/main/webapp/resources/js/memberRegister.js
+++ project/src/main/webapp/resources/js/memberRegister.js
@@ -2,7 +2,7 @@
  * register 스크립트 부분 처리 완료
  */
 
-//이용약관 동의
+// 이용약관 동의
 var doc = document;
 var form1 = doc.getElementById('form1');
 var inputs = form1.getElementsByTagName('INPUT');
@@ -21,7 +21,7 @@ var c3 = doc.getElementById('c3');
 function checkboxListener() {
 	form1_data[this.name] = this.checked;
 	if (this.checked) {
-		// submit 할때 체크하지 않아 색이 변한 font 를 다시 원래 색으로 바꾸는 부분. 
+		// submit 할때 체크하지 않아 색이 변한 font 를 다시 원래 색으로 바꾸는 부분.
 		this.parentNode.style.color = "#000";
 	}
 }
@@ -45,7 +45,7 @@ function setCheckbox(obj, state) {
 	}
 }
 
-//비밀번호 체크
+// 비밀번호 체크
 $(function() {
 
 	var finalSignal2 = false; // 약관 필수 체크
@@ -70,10 +70,11 @@ $(function() {
 		}
 	});
 
-	//닉네임 체크
+	// 닉네임 체크 및 핸드폰 번호
 	$('#nicknameCkeck').click(function() {
 		var nicknameControl = true;
 		var nickname = $('#nickname');
+
 		if (nickname.val() == '') {
 			alert('공백은 허용 되지 않습니다.');
 			nickname.focus();
@@ -104,30 +105,51 @@ $(function() {
 		}
 	})
 
-	//finalSignal 처리 함수
+	// finalSignal 처리 함수
 	function finalSignal() {
 		if (form1_data["c1"] && form1_data["c2"]) {
-			alert("c1:" + form1_data["c1"]);
-			alert("c2:" + form1_data["c2"]);
+			/*
+			 * alert("c1:" + form1_data["c1"]); alert("c2:" + form1_data["c2"]);
+			 */
 			finalSignal2 = true;
 		}
 	}
 
 	var formObj = $("form[role='form']");
-	//회원 가입
+	var phoneNull = true;
+	// 회원 가입
 	$('#submit').click(
 			function() {
-				finalSignal();
-				alert(finalSignal2);
-				alert(nickNameCheck);
-				alert(passwordComfirm);
-				if (finalSignal2 == true && nickNameCheck == true
-						&& passwordComfirm == true) {
-					alert("모두 완료");
-					formObj.submit();
+				var phone = $('#phone');
+				if (phone.val() == '') {
+					alert('핸드폰 공백을 허용하지 않습니다.');
+					phoneNull = false;
+					phone.focus();
 				} else {
-					alert("체크가 덜 됐다");
+					phoneNull = true;
 				}
+
+				finalSignal();
+				/*
+				 * alert(finalSignal2); alert(nickNameCheck);
+				 * alert(passwordComfirm);
+				 */
+				if (nickNameCheck == false) {
+					alert("닉네임 중복확인 하세요");
+				}
+				if (passwordComfirm == false) {
+					alert("비밀번호가 일치하는지 다시한번 확인해봐요");
+				}
+				if(finalSignal2 == false){
+					alert("필수 약관을 체크 하셔야 합니다.");
+				}
+
+				if (finalSignal2 == true && nickNameCheck == true
+						&& passwordComfirm == true && phoneNull == true) {
+					/* alert("모두 완료"); */
+					formObj.submit();
+				}
+
 			})
 
 });
