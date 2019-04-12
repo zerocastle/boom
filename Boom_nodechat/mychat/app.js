5f5b7f5e57ab4539ac66ef5cc5ed2ce599f86407
Jackson-Kim <43841846+Jackson-Kim@users.noreply.github.com>
2019-04-12 19:11:33 +0900

    사이드메뉴바 및 채팅방나가기 구현
    사이드메뉴바 움직임 적용 및
    채팅방 나가기 기능 구현.
    채팅방 나기기를 할때
    DB의 해당 채팅방의 row를 삭제함과 동시에 연관된 메시지테이블의 row들을 삭제한다.

--- Boom_nodechat/mychat/app.js
+++ Boom_nodechat/mychat/app.js
@@ -1,24 +1,22 @@
 /* socket\room_chat\app.js */
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-var redis = require("redis");
-const v = require('voca');
-var session = require('express-session');
-var client;
-var testdata;
-app.use(session({
+var express = require('express'); // express 서버 import
+var app = express(); // 서버 객체 생성
+const http = require('http').Server(app); // http 통신규약 import
+const io = require('socket.io')(http); // socket.io import
+var redis = require("redis"); //redis import
+var session = require('express-session'); //express-session import
+var client; //redis의 connection 객체가 될 변수
+app.use(session({ // 세션사용을 위한 절차
 	secret: '12sdfwerwersdfserwerwef', //keboard cat (랜덤한 값)
 	resave: false,
 	saveUninitialized: true
 }));
+app.use(express.static(__dirname + '/public')); // resource파일들의 경로설정을 위한 절차
+app.set('view engine', 'ejs'); //뷰 템플릿 지정. .ejs 로 작성되어야한다. 
+app.set('views', './views'); //경로지정. view단의 파일들은 해당 경로에 저장되어야 한다.
 
 
-app.set('view engine', 'ejs');
-app.set('views', './views');
-
-
-let room = ['room1', 'room2'];
+let room = [];//방 객체 테스트용으로 만듬
 let a = 0;
 
 var conn;
@@ -65,70 +63,63 @@ app.get('/roomchat', (req, res) => {
   });
 });
 
+app.get('/menutest', (req, res) => {
+  res.render('test');
+});
 
 
 
 
 
 // 방목록 불러오기
-//select room_id, room_title from test_room where buyer_id = 'jack' or seller_id = 'jack';
 app.get('/jackchat', (req, res) => {//localhost:3000/jackchat 으로 접근시 실행
   client = redis.createClient(6379, "localhost");//localhost6379포트의 redis객체에 접근한다.
-    client.get("user", function(err, val) {//redis객체에 "user"라는 키의 값을 찾아 함수실행
-    testdata=val;//해당 키의 값을 넣어주고
+    client.get("user", function(err, val) {//스프링에서 저장한 redis객체에 "user"라는 키의 값을 찾아 함수실행
+    testdata=val;
     req.session.nickname = val;
-    var arr = [];
+    console.log('찍어봅시다 : ' , val) // 채팅서버에 접속한 유저의 nickname을 찍어본다.
+    
     if(val === null) {//값이 없다면 요거하고 땡
       console.log('>>>>> result : null ');
     }
     else {//값이 있다면 실행 select c.room_id, c.buyer_num, c.seller_num, c.pro_num, o.title        from chatroom c, production o where o.pro_num = c.pro_num and (      seller_num = (select m_num from member where nickname = 'jackson') or      buyer_num = (select m_num from member where nickname = 'jackson'));
     //select c.room_id, (select nickname from member where m_num = c.buyer_num), (select nickname from member where m_num = c.seller_num), c.pro_num, o.title from chatroom c, production o where o.pro_num = c.pro_num and (      seller_num = (select m_num from member where nickname = 'tom') or      buyer_num = (select m_num from member where nickname = 'tom'));
-      var loglogsql = "select c.room_id, c.buyer_num, c.seller_num, c.pro_num, o.title, (select nickname from member where m_num = c.buyer_num) C_buyer_nickname, (select nickname from member where m_num = c.seller_num) C_seller_nickname  from chatroom c, production o where o.pro_num = c.pro_num and (      seller_num = (select m_num from member where nickname = '"+val+"') or      buyer_num = (select m_num from member where nickname = '"+val+"'))";
+      var loglogsql = "select c.room_id, c.buyer_num, c.seller_num, c.pro_num, o.title, (select nickname from member where m_num = c.buyer_num) C_buyer_nickname, (select nickname from member where m_num = c.seller_num) C_seller_nickname  from chatroom c, production o where o.pro_num = c.pro_num and (      seller_num = (select m_num from member where nickname = '"+val+"') or      buyer_num = (select m_num from member where nickname = '"+val+"')) order by room_id asc";
       //conn.execute("select room_id, room_title from test_room where buyer_id = '"+val+"' or seller_id = '"+val+"'",
       conn.execute(loglogsql,
       function(err, result){
+        if(result == 'undefined'){
+            console.log('결과값이 비어있어 실행이 정상동작하지 않습니다.')
+        }
+        
+      //conn.execute("select room_id, room_title from test_room where buyer_id = 'tom' or seller_id = 'tom'",function(err,result){ 이게 실행이됩니다.
+        else if(err){
+            console.log("/jackchat : 등록중 에러가 발생했어요!! ", err);
+        }else if(!result.rows.length){
         console.log("result: 리저어어얼트",result);
         console.log("result.rows: 리저어어얼트로우",result.rows);
         console.log("result의 길이값 @@@@@@@@@@   "+result.rows.length);
-      //conn.execute("select room_id, room_title from test_room where buyer_id = 'tom' or seller_id = 'tom'",function(err,result){ 이게 실행이됩니다.
-        if(err){
-            console.log("/jackchat : 등록중 에러가 발생했어요!! ", err);
-        }else if(!result.rows.length){
             console.log("/jackchat : ROws가 0이다. ");
             res.render('roomlist',{result:JSON.stringify(result),nickname:req.session.nickname}); 
         }else{
-            // 
-            // result는 JSON Type. metaData, rows 라는 key를 가지고 있다. {metaData는 다룰일이 없어 기술하지않음}
+            
+            // result는 JSON으로 전달되며 . metaData, rows 라는 key를 가지고 있다. {metaData는 다룰일이 없어 기술하지않음}
             // [metaData:{key:value}][rows:['1301015','김재근','010-4241-1101']['1600243','박땡땡','010-4447-2663']...]
             // result.rows는 select문의 결과집합을 가지고 있으며 이중배열의 형태로 반환된다.
             // result.rows       --> [['1301006' ,'김재근'], ['1500222', '박뭐뭐'], ['1711111', '하하']
             // result.rows[0]    --> ['1301006', '김재근'] 와 같은 형식
             // result.rows[1][1] --> ['박뭐뭐']
             // result.rows[x][y] --> x : row를 선택,  y:column을 선택
-            /*
-            var jsonoobj = {};
-            for(var i = 0; i < result.rows.length; i++){
-              jsonoobj = {
-                  room_id : result.rows[i][0],
-                  room_title : result.rows[i][1]
-              }
-              console.log("JSON으로 만듬 : ", jsonoobj);
-              var realJson = JSON.parse(JSON.stringify(jsonoobj));
-              var stringjack = JSON.stringify(jsonoobj);
-              arr.push(realJson);
-              console.log("arr로 만듬 : ", arr);
-            }
-            console.log("JSON배열화 종료", arr);*/
               
-              res.render('roomlist',{result:JSON.stringify(result),nickname:req.session.nickname});         //  res.render('roomlist', arr);
+              res.render('roomlist',{result:JSON.stringify(result),nickname:req.session.nickname});        
         }//DB쿼리문- if else 
-      });//if else- redis의 값이 없는게 아니라면
+      });//if else- redis의 값
       }//client.get 함수
     });//redis create 함수
 });//app.get함수
 
-io.on('connection', (socket) => {
-  socket.on('disconnect', () => {
+io.on('connection', (socket) => {//socketIO연결이 되며 소켓에 전송되는 문자열이 일치하는 메소드를 실행한다.
+  socket.on('disconnect', () => {//socket에 disconnect라는 메시지가 전송되면 실행
     console.log('user disconnected');
   });
 
@@ -141,21 +132,37 @@ io.on('connection', (socket) => {
   });
 
 
+  socket.on('room_out', (num,name) => {
+    console.log(name+'회원이 No.'+num+'방을 나가셨습니다.' );
+    var room_outSql = "delete from chatroom where room_id = "+num+" ";
+    //입장한 채팅방을 나가며 연관된 메시지 테이블의 내용도 일부 제거된다.
+    
+    var change_countSql = "update production set chat_room_count = chat_room_count -1  where pro_num = (select pro_num from chatroom where room_id = "+num+")";
+    //상품테이블의 채팅방 갯수를 줄인다. - (방번호를 이용해 방의 상품번호를 알아낸다.) - 알아낸 상품번호의 채팅방갯수를 하나 줄인다.
+    
+    conn.execute(change_countSql,function(err,result){//업데이트문 실행
+      if(err){
+        console.log("change_countSql 에러 :",err);
+      } else {  
+      console.log('상품 테이블에 채팅방갯수 줄이기', result);
+      }
+    });
+
+    conn.execute(room_outSql,function(err,result){//딜리트문 실행
+      if(err){
+        console.log("room_outSql 에러 :",err);
+      } else {
+      console.log('채팅방 지우기' , result);
+    }      
+    });
+
+
+    io.to(room[num]).emit('room_out', num, name);
+  });
+
   socket.on('joinRoom', (num, name) => {
     socket.join(room[num], () => {
-      console.log(name + ' join a ' + room[num]);
-      /*  //방입장시 하는건데 필요없음
-      conn.execute("insert into test_user(USER_ID) values('"+name+"')",function(err,result){
-        if(err){
-            console.log("등록중 에러가 발생했어요!!", err);
-           
-        }else{
-            console.log("result : ",result);
-          
-        }
-      });
-      */
-
+      console.log(name + ' join a ' + num);
       io.to(room[num]).emit('joinRoom', num, name);
     });
   });
