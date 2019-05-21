d2a9077da14177ec983fd72bbaeb344f34aec68c
zerocastle <37466763+zerocastle@users.noreply.github.com>
2019-05-21 11:06:46 +0900

    0521 점심시간 전 커밋

---
+++ project/src/main/java/com/ys/project/controller/member/SearchController.java
@@ -0,0 +1,11 @@
+package com.ys.project.controller.member;
+
+import org.springframework.stereotype.Controller;
+
+import lombok.AllArgsConstructor;
+
+@Controller
+@AllArgsConstructor
+public class SearchController {
+
+}
