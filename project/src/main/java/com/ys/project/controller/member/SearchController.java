b73a59f0d397bd78dc00b9aea7840a7fb78e11ab
zerocastle <37466763+zerocastle@users.noreply.github.com>
2019-05-21 14:39:03 +0900

    상품 와리가리 , 상품 리스트 구별 , 인덱스 페이지 날짜순 , 그리고 검색 하기전

--- project/src/main/java/com/ys/project/controller/member/SearchController.java
+++ project/src/main/java/com/ys/project/controller/member/SearchController.java
@@ -1,11 +1,36 @@
 package com.ys.project.controller.member;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ys.project.projectDTO.IndexProductionDTO;
+import com.ys.project.service.production.IProductionService;
 
 import lombok.AllArgsConstructor;
 
-@Controller
+@RestController
 @AllArgsConstructor
 public class SearchController {
+
+	private static final Logger log = LoggerFactory.getLogger(SearchController.class);
+
+	private IProductionService service;
+	
+	@GetMapping(value = "/search/{pageNum}/{perPage}/{choose}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public void search(@PathVariable("pageNum") int pageNum, @PathVariable("perPage") int perPage,
+			@PathVariable("choose") String choose) {
+		
+		log.info("넘오온 값" + perPage + "" + choose);
+		
+		List<IndexProductionDTO> dto = service.searchSort(pageNum, perPage, choose);
+		
+
+	}
 
 }
