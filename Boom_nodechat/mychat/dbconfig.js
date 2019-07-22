f6d5e9f81f5e5ec2f670646c6b509eb3c0019743
Jackson-Kim <43841846+Jackson-Kim@users.noreply.github.com>
2019-07-22 15:04:00 +0900

    채팅방 디자인 적용 및 상대이미지 출력
    채팅방 디자인 적용 및 상대이미지 출력
    nodejs만 수정됨

--- Boom_nodechat/mychat/dbconfig.js
+++ Boom_nodechat/mychat/dbconfig.js
@@ -69,16 +69,16 @@
  *****************************************************************************/
 
 module.exports = {
-    user          : process.env.NODE_ORACLEDB_USER || "kys",
+    user          : process.env.NODE_ORACLEDB_USER || "tom",
   
     // Instead of hard coding the password, consider prompting for it,
     // passing it in an environment variable via process.env, or using
     // External Authentication.
-    password      : process.env.NODE_ORACLEDB_PASSWORD || "kys",
+    password      : process.env.NODE_ORACLEDB_PASSWORD || "tom",
   
     // For information on connection strings see:
     // https://oracle.github.io/node-oracledb/doc/api.html#connectionstrings
-    connectString : process.env.NODE_ORACLEDB_CONNECTIONSTRING || "bon300-11/orcl",
+    connectString : process.env.NODE_ORACLEDB_CONNECTIONSTRING || "bon300-8/orcl",
   
     // Setting externalAuth is optional.  It defaults to false.  See:
     // https://oracle.github.io/node-oracledb/doc/api.html#extauth
